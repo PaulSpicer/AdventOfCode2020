0be53327f58cb6ea21651156112c3724357fc930
Paul <Ozukki@hotmail.co.uk>
2020-12-04 09:14:35 +0000

    Completed Days 2 & 3

---
+++ Day 2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rawPasswords = ReadInputFileToArray();
+            int noOfValidPasswords = 0, noOfValidPasswords2 = 0;
+            
+            for (int i = 0; i < rawPasswords.Length; i++)
+            {
+                var pw = new PasswordData(rawPasswords[i]);
+                if (pw.IsPasswordValid())
+                    noOfValidPasswords++;
+
+                if (pw.IsPasswordValid2())
+                    noOfValidPasswords2++;
+            }
+            
+            Console.WriteLine($"There are {noOfValidPasswords} valid passwords for part 1");
+            Console.WriteLine($"There are {noOfValidPasswords2} valid passwords for part 2");
+        }
+        
+        private static string[] ReadInputFileToArray()
+        {
+            return File.ReadAllLines("input.txt");
+        }
+    }
+
+    class PasswordData
+    {
+        private int Minimum { get; set; }
+        private int Maximum { get; set; }
+        private char Letter { get; set; }
+        private string Password { get; set; }
+
+        public PasswordData (string rawInput)
+        {
+            var dashIndex = rawInput.IndexOf('-');
+            var colonIndex = rawInput.IndexOf(':');
+
+            Minimum = int.Parse(rawInput.Substring(0, dashIndex));
+            Maximum = int.Parse(rawInput.Substring(dashIndex + 1,  colonIndex - dashIndex - 3));
+            Letter = char.Parse(rawInput.Substring(colonIndex - 1, 1));
+            Password = rawInput.Substring(colonIndex + 2, rawInput.Length - colonIndex - 2);
+        }
+
+        public bool IsPasswordValid()
+        {
+            var count = 0;
+            foreach (char c in Password)
+            {
+                if (c == Letter)
+                {
+                    count++;
+                }
+            }
+            return count >= Minimum && count <= Maximum;
+        }
+        public bool IsPasswordValid2()
+        {
+            return (Password[Minimum - 1] == Letter || Password[Maximum - 1] == Letter) && Password[Minimum - 1] != Password[Maximum - 1];
+        }
+    }
+}
---
+++ Day 3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var treeData = ReadInputFileToArray();
+
+            long multipliedTrees = 1;
+
+            var runs = new (int  xOffset, int yOffset)[]
+            {
+                (1,1),
+                (3,1),
+                (5,1),
+                (7,1),
+                (1,2)
+            };
+
+            foreach (var r in runs)
+            {
+                int trees = 0;
+                var xPosition = 0;
+                for (int i = 0; i < treeData.Length; i += r.yOffset )
+                {
+                    if (treeData[i][xPosition] == '#')
+                    {
+                        trees++;
+                    }
+                    
+                    xPosition += r.xOffset;
+                    xPosition %= treeData[i].Length;
+                }
+
+                multipliedTrees *= trees;
+            }
+            
+            Console.Write($"There are {multipliedTrees} trees.");
+        }
+        
+        private static string[] ReadInputFileToArray()
+        {
+            return File.ReadAllLines("input.txt");
+        }
+    }
+}
